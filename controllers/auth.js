4e4af89753d88067589e79bbcf046d6f56bf2dee
rafaelrendonx <rafael_x@hotmail.com>
2022-11-18 13:30:09 -0700

    Se agrega Login

---
+++ controllers/auth.js
@@ -0,0 +1,37 @@
+const Usuarios = require("../models/usuarios")
+
+const register = async () => {
+
+}
+
+const login = async (req, res) => {
+    const { correo, password } = req.body
+
+    try {
+        const usuario = await Usuarios.findOne({ correo })
+
+        if (!usuario) {
+            return res.status(400).json({
+                mensaje: "Usuario no encontrado",
+            })
+        }
+        
+        const validPassword = () => {
+            return password === usuario.password
+        }
+
+        if (!validPassword) {
+            return res.status(400).json({
+                mensaje: "Password Incorrecto",
+            })
+        }
+
+        res.json({
+            mensaje: "Ok, usuario logeado"
+        })
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
